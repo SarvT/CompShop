ebf6156cdc11d808dd0296fd8ab6dae587f9c2f4
SarvT <91484197+SarvT@users.noreply.github.com>
2023-06-18 15:55:46 +0530

    A start. HomePage commit.

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Navbar';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Welcome To CompShop!</h1>
+    </div>
+  );
+}
+// export default Home;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <a href="/" class="navbar-brand text-light">CompShop</a>
+        
+      </nav>
+    </div>
+  )
+}
